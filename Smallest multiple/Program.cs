68587032fdd6701b5e77e2ee728ada8bf8144fc8
Filip Figuli <figuli.filip@gmail.com>
2023-04-05 21:23:54 +0200

    Solved: Smallest multiple

---
+++ Smallest multiple/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Smallest_multiple;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 2520;
+
+        // Iterate over numbers until we find one that will be divisible
+        while (!IsDivisible(number))
+        {
+            number++;
+        }
+
+        Console.WriteLine(number);
+    }
+
+    // Check if number is divisible by all numbers 1..20
+    static bool IsDivisible(int number)
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            if (number % i != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
