94e7bcb01a281ce471a016f462f6932bd0374cf5
Filip Figuli <figuli.filip@gmail.com>
2023-04-05 20:06:44 +0200

    Solved: Multiples of 3 or 5

---
+++ Multiples of 3 or 5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Multiples_of_3_or_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+
+        // Iterate through all numbers belowe 1000
+        for (int i = 1; i < 1000; i++)
+        {
+            // If i is multiple of 3 or 5 add it to the sum
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
