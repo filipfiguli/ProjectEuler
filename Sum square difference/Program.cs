af9a247120d146448661423389d88d288165c688
Filip Figuli <figuli.filip@gmail.com>
2023-04-05 21:31:41 +0200

    Solved: Sum square difference

---
+++ Sum square difference/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sum_square_difference;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long squareOfSum = 0;
+        long sumOfSquares = 0;
+
+        for (long i = 1; i <= 100; i++)
+        {
+            sumOfSquares += (long) Math.Pow(i, 2);
+            squareOfSum += i;
+        }
+
+        Console.WriteLine(Math.Pow(squareOfSum, 2) - sumOfSquares);
+    }
+}
+
